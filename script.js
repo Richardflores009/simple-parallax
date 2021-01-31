a90d06920e7148b5bfc167e6353425ed61c0a993
Dick Flowers <dickflowers@DickMiniPro.local>
2021-01-31 10:59:34 -0700

    added y and x access scroll to span and vertical scroll for li

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 window.addEventListener('scroll',function(e){
 
-    const target = document.querySelector('.scroll')
+    const target = document.querySelectorAll('.scroll')
 
-    var scrolled = window.pageYOffset;
-    var rate = scrolled * .5;
+    // var scrolled = window.pageYOffset;
+    // var rate = scrolled * .5;
 
-    target.style.transform = 'translate3d(0px,'+rate+'px, 0px)'
+    // target.style.transform = 'translate3d(0px,'+rate+'px, 0px)'
+
+    for (let i = 0; i < target.length; i++) {
+        var pos = window.pageYOffset * target[i].dataset.rate;
+
+        if(target[i].dataset.direction === 'vertical') {
+            target[i].style.transform = 'translate3d(0px, '+pos+'px, 0px)'
+        } else {
+            var posX = window.pageYOffset * target[i].dataset.ratex;
+            var posY = window.pageYOffset * target[i].dataset.ratey;
+            
+            target[i].style.transform = `translate3d(${posX}px, ${posY}px, 0px)`;
+        }
+    }
 })
